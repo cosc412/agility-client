73abb9355b30fac218b663f6df1d98f6aecec85e
EVogel1999 <eav990@gmail.com>
2019-03-13 16:37:10 -0400

    Add authorization logic to navbar

--- agility/src/app/shared/top-navbar/top-navbar.component.ts
+++ agility/src/app/shared/top-navbar/top-navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-top-navbar',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavbarComponent implements OnInit {
 
-  constructor() { }
+  auth2: any;
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  /**
+   * Authenticates google sign in and attaches click handler to sign in button
+   */
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '675904009128-7qot95l5mnb63pbq98d6u7ab0djshgsj.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('nav_sign_in'));
+    });
+  }
+
+  /**
+   * Attaches a google sign in click handler to buttons given the button id
+   * Sets the profile of auth service to returned profile
+   * @param element Button id name
+   */
+  attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+        let profile = googleUser.getBasicProfile();
+        this.auth.profile = profile;
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+  }
+
+  ngAfterViewInit(){
+    this.googleInit();
   }
 
 }
