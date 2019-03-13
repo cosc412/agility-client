a0e374b09fe64a2dfa18ac1e05ce81642c432d73
EVogel1999 <eav990@gmail.com>
2019-03-12 23:25:40 -0400

    Route to home page

--- agility/src/app/app-routing.module.ts
+++ agility/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SplashComponent } from './home/splash/splash.component';
+
+const routes: Routes = [
+  { path: 'home', component: SplashComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
