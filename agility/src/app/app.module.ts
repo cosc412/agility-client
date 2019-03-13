c35df2afea65a63d8def93b551be1e69a65b41ba
EVogel1999 <eav990@gmail.com>
2019-03-12 22:50:18 -0400

    Create and style navbar and footer

--- agility/src/app/app.module.ts
+++ agility/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './shared/top-navbar/top-navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
