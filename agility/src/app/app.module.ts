c5a518785a6871224943ca03b2e8f91ce281e8c1
EVogel1999 <eav990@gmail.com>
2019-03-12 23:30:52 -0400

    Create config for google login

--- agility/src/app/app.module.ts
+++ agility/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './shared/top-navbar/top-navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SplashComponent } from './home/splash/splash.component';
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('675904009128-obnvbfu2c20f4oi81m96f6bl2pumkca2.apps.googleusercontent.com')
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
