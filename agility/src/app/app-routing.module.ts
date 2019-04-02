675081001960f91b9ef82b734534a92d04e71c8d
EVogel1999 <eav990@gmail.com>
2019-04-02 16:02:46 -0400

    Link projects button to project dash

--- agility/src/app/app-routing.module.ts
+++ agility/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SplashComponent } from './home/splash/splash.component';
+import { ProjectDashComponent } from './dashboard/project-dash/project-dash.component';
 
 const routes: Routes = [
   { path: 'home', component: SplashComponent },
+  { path: 'projects', component: ProjectDashComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
