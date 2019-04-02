aec6fd8aad69c78c3245a0b4c21a84ee9cfc07fc
EVogel1999 <eav990@gmail.com>
2019-04-02 16:13:15 -0400

    Create mock project data

--- agility/src/app/auth/project.service.ts
+++ agility/src/app/auth/project.service.ts
@@ -5,5 +5,42 @@ import { Injectable } from '@angular/core';
 })
 export class ProjectService {
 
+  mock_projects = [
+    {
+      _id: 1,
+      name: `John Doe's Project`,
+      description: 'This is a very descriptive project!'
+    },
+    {
+      _id: 2,
+      name: `Jane Doe's Project`,
+      description: 'This is a very descriptive project!'
+    },
+    {
+      _id: 3,
+      name: `Emily Vogel's Project`,
+      description: 'This is a very descriptive project!'
+    },
+    {
+      _id: 4,
+      name: `Cameron Crow's Project`,
+      description: 'This is a very descriptive project!'
+    },
+    {
+      _id: 5,
+      name: `Sam Salas's Project`,
+      description: 'This is a very descriptive project!'
+    },
+    {
+      _id: 6,
+      name: `Andrew Noonan's Project`,
+      description: 'This is a very descriptive project!'
+    }
+  ]
+
   constructor() { }
+
+  getProjects(userID: string) {
+    return this.mock_projects;
+  }
 }
