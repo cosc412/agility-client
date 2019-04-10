7b70a79e07e3708f12b8249522f21ae97730e842
EVogel1999 <eav990@gmail.com>
2019-04-09 20:28:07 -0400

    Route to components in the navbar

--- agility/src/app/app-routing.module.ts
+++ agility/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SplashComponent } from './home/splash/splash.component';
 import { ProjectDashComponent } from './dashboard/project-dash/project-dash.component';
+import { FaqPageComponent } from './home/faq-page/faq-page.component';
 
 const routes: Routes = [
   { path: 'home', component: SplashComponent },
+  { path:'faq', component: FaqPageComponent },
   { path: 'projects', component: ProjectDashComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
