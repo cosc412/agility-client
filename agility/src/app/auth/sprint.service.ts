c9c33982b5ad0017c3e312e454e72f40c405485d
EVogel1999 <eav990@gmail.com>
2019-04-10 10:13:46 -0400

    Scaffold sprint service

--- agility/src/app/auth/sprint.service.ts
+++ agility/src/app/auth/sprint.service.ts
@@ -5,5 +5,54 @@ import { Injectable } from '@angular/core';
 })
 export class SprintService {
 
+  private MOCK_SPRINTS: {_id: string, projID: string, header: string, due: string, description: string}[] = [
+    {
+      _id: '1',
+      projID: '1',
+      header: 'This is a new sprint!',
+      due: '4/10/2019',
+      description: 'We need to complete this sprint TODAY'
+    },
+    {
+      _id: '2',
+      projID: '1',
+      header: 'This is a new sprint!',
+      due: '4/10/2019',
+      description: 'We need to complete this sprint TODAY'
+    },
+    {
+      _id: '3',
+      projID: '1',
+      header: 'This is a new sprint!',
+      due: '4/10/2019',
+      description: 'We need to complete this sprint TODAY'
+    },
+    {
+      _id: '4',
+      projID: '1',
+      header: 'This is a new sprint!',
+      due: '4/10/2019',
+      description: 'We need to complete this sprint TODAY'
+    }
+  ]
+
   constructor() { }
+
+  getProjectSprints(projID: string) {
+    return this.MOCK_SPRINTS.filter(val => {
+      return val.projID === projID;
+    });
+  }
+
+  createProjectSprint(projID: string, data) {
+
+  }
+
+  updateProjectSprint(projID: string, data) {
+
+  }
+
+  deleteProjectSprint(sprintID: string) {
+    
+  }
 }
