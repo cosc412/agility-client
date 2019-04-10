f0c168439819aa653e73f0b9b03ec9493fe7eaab
EVogel1999 <eav990@gmail.com>
2019-04-09 21:18:17 -0400

    Style qa view

--- agility/src/app/shared/qa-view/qa-view.component.ts
+++ agility/src/app/shared/qa-view/qa-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-qa-view',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QaViewComponent implements OnInit {
 
+  @Input() question;
+  expand = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  expandMenu() {
+    this.expand = !this.expand;
   }
 
 }
