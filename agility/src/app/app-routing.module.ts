5690018c9f8d77b77153d7c861469a8c1d8d74f2
EVogel1999 <eav990@gmail.com>
2019-04-24 16:19:34 -0400

    Set up guard to stop non priviledged users

--- agility/src/app/app-routing.module.ts
+++ agility/src/app/app-routing.module.ts
@@ -8,13 +8,15 @@ import { ProjectDetailsDashComponent } from './dashboard/project-details-dash/pr
 import { TeamPageComponent } from './dashboard/project-details-dash/team-page/team-page.component';
 import { TaskDetailsComponent } from './dashboard/project-details-dash/task-details/task-details.component';
 
+import { ProjectGuard } from './guards/project.guard';
+
 const routes: Routes = [
   { path: 'home', component: SplashComponent },
   { path:'faq', component: FaqPageComponent },
   { path: 'projects', component: ProjectDashComponent },
-  { path: 'projects/:id', component: ProjectDetailsDashComponent },
-  { path: 'projects/:id/team', component: TeamPageComponent },
-  { path: 'projects/:id/tasks/:taskID', component: TaskDetailsComponent },
+  { path: 'projects/:id', component: ProjectDetailsDashComponent, canActivate: [ProjectGuard] },
+  { path: 'projects/:id/team', component: TeamPageComponent, canActivate: [ProjectGuard] },
+  { path: 'projects/:id/tasks/:taskID', component: TaskDetailsComponent, canActivate: [ProjectGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
--- agility/src/app/guards/project.guard.ts
+++ agility/src/app/guards/project.guard.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProjectGuard implements  {
-  
+export class ProjectGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot) {
+    if (!this.auth.user)
+      return false;
+    else {
+      this.auth.getMyProjectRole(route.params.id).then(val => {
+        if (val)
+          return true;
+        else {
+          this.router.navigate(['']);
+          return false;
+        }
+      }).catch(e => {
+        return false;
+      });
+    }
+  }
 }
