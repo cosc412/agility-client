c1ebf21dc79d3a9920da8afa37666176119b3d06
EVogel1999 <eav990@gmail.com>
2019-04-24 14:41:31 -0400

    Style add user popup

--- agility/src/app/shared/user-popup/user-popup.component.ts
+++ agility/src/app/shared/user-popup/user-popup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 
 @Component({
   selector: 'app-user-popup',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPopupComponent implements OnInit {
 
-  constructor() { }
+  model = {
+    email: ''
+  };
+
+  constructor(private dialogRef: MatDialogRef<UserPopupComponent>, @Inject(MAT_DIALOG_DATA) data) { }
 
   ngOnInit() {
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+
+  add() {
+
   }
 
 }
